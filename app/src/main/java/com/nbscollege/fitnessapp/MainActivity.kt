dbf0f7a70a7112cb4d5e3750129961700825d75b
Azzelswee <147906039+Azzelswee@users.noreply.github.com>
2023-10-21 16:41:24 +0800

    SignUp Screen

--- app/src/main/java/com/nbscollege/fitnessapp/MainActivity.kt
+++ app/src/main/java/com/nbscollege/fitnessapp/MainActivity.kt
@@ -10,6 +10,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.example.model.LogInScreen
+import com.example.example.model.SignUpScreen
 import com.nbscollege.fitnessapp.ui.theme.FitnessAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,29 +20,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             FitnessAppTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                var log = SignUpScreen()
+                log.SignUp()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FitnessAppTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/com/nbscollege/fitnessapp/SignUpScreen.kt
+++ app/src/main/java/com/nbscollege/fitnessapp/SignUpScreen.kt
@@ -17,13 +17,18 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.KeyboardArrowLeft
 import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Phone
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -43,33 +48,40 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.nbscollege.fitnessapp.R
 
 
-class LogInScreen {
+class SignUpScreen {
 
 
-    @OptIn(ExperimentalMaterial3Api::class)
+    @OptIn(ExperimentalMaterial3Api::class, ExperimentalComposeUiApi::class)
     @Composable
-    fun Login() {
+    fun SignUp() {
+        var name by remember { mutableStateOf("") }
         var email by remember { mutableStateOf("") }
+        var phoneNumber by remember { mutableStateOf("") }
         var password by remember { mutableStateOf("") }
-        var rememberMe by remember { mutableStateOf(false) }
+        var agree by remember { mutableStateOf(false) }
         var passwordVisibility by remember { mutableStateOf(false) }
         val roundedCornerShape: Shape = RoundedCornerShape(16.dp)
+        val keyboardController = LocalSoftwareKeyboardController.current
 
         val textFieldColors = TextFieldDefaults.textFieldColors(
 
@@ -95,7 +107,7 @@ class LogInScreen {
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(top = 20.dp)
-                    .padding(horizontal = 16.dp),
+                    .padding(),
                 horizontalArrangement = Arrangement.SpaceBetween
             ) {
                 // Back icon
@@ -108,35 +120,64 @@ class LogInScreen {
                         imageVector = Icons.Default.KeyboardArrowLeft,
                         contentDescription = "Back",
                         tint = Color.Gray,
-                        modifier = Modifier.size(32.dp)
+                        modifier = Modifier.size(40.dp)
                     )
                 }
 
             }
 
             Image(
-                painter = painterResource(R.drawable.fitnesslogo),
+                painter = painterResource(id = R.drawable.fitnesslogo), // Replace with your image resource
                 contentDescription = null, // Provide a content description
                 modifier = Modifier
-                    .size(width = 200.dp, height = 150.dp)
+                    .size(width = 250.dp, height = 200.dp)
                     .align(Alignment.CenterHorizontally)
 
             )
 
-            Spacer(modifier = Modifier.height(10.dp)) // Add vertical spa
-
-
             Text(
-                text = "Log in",
-                color = Color.Black, fontSize = 18.sp
+                text = "Hey There,",
+                color = Color.Black, fontSize = 21.sp
             )
             Text(
-                text = "Welcome Back",
+                text = "Create an Account",
                 color = Color.Black,
                 fontSize = 35.sp
             )
 
-            Spacer(modifier = Modifier.height(75.dp)) // Add vertical spa
+            Spacer(modifier = Modifier.height(40.dp)) // Add vertical spa
+            TextField(
+                value = name,
+                onValueChange = {
+                    name = it
+                },
+
+
+                textStyle = TextStyle(
+                    fontSize = 16.sp,
+                    color = Color.Black
+                ),
+                label = { Text("Full name", color = Color.Gray) },
+                trailingIcon = {
+                    Icon(
+                        Icons.Default.Person, contentDescription = "Contact icon",
+                        modifier = Modifier.graphicsLayer(alpha = 0.5f)
+                    ) // Add email icon here
+                },
+                singleLine = true,
+                shape = roundedCornerShape,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 20.dp)
+                    .padding(end = 20.dp),
+                colors = textFieldColors,
+
+
+
+
+                )
+            Spacer(modifier = Modifier.height(15.dp)) // Add vertical space
+
             TextField(
                 value = email,
                 onValueChange = {
@@ -148,7 +189,7 @@ class LogInScreen {
                     fontSize = 16.sp,
                     color = Color.Black
                 ),
-                label = { Text("Email", color = Color.Gray) },
+                label = { Text("Password", color = Color.Gray) },
                 trailingIcon = {
                     Icon(
                         Icons.Default.Email, contentDescription = "Email icon",
@@ -163,11 +204,44 @@ class LogInScreen {
                     .padding(end = 20.dp),
                 colors = textFieldColors,
 
-
                 )
 
             Spacer(modifier = Modifier.height(15.dp)) // Add vertical space
 
+
+            TextField(
+                value = phoneNumber,
+                onValueChange = {
+                    phoneNumber = it
+                },
+
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Phone,
+                    imeAction = ImeAction.Done
+                ),
+
+
+                textStyle = TextStyle(
+                    fontSize = 20.sp,
+                    color = Color.Black
+                ),
+                label = { Text("Weight", color = Color.Gray) },
+                trailingIcon = {
+                    Icon(
+                        Icons.Default.Phone, contentDescription = "Password lock icon",
+                        modifier = Modifier.graphicsLayer(alpha = 0.5f)
+                    ) // Add lock icon here
+                },
+                singleLine = true,
+                shape = roundedCornerShape,
+                modifier = Modifier
+
+                    .fillMaxWidth()
+                    .padding(start = 20.dp)
+                    .padding(end = 20.dp), colors = textFieldColors
+
+            )
+            Spacer(modifier = Modifier.height(15.dp)) // Add vertical space
 
             TextField(
                 value = password,
@@ -175,12 +249,16 @@ class LogInScreen {
                     password = it
                 },
 
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Password,
+                    imeAction = ImeAction.Done
+                ),
 
                 textStyle = TextStyle(
                     fontSize = 20.sp,
                     color = Color.Black
                 ),
-                label = { Text("Password", color = Color.Gray) },
+                label = { Text("Height", color = Color.Gray) },
                 trailingIcon = {
                     Icon(
                         Icons.Default.Lock, contentDescription = "Password lock icon",
@@ -203,53 +281,60 @@ class LogInScreen {
             Row(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(), verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.SpaceBetween
+                    .padding(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
             ) {
-                // Remember Me Checkbox
 
-                Row(verticalAlignment = Alignment.CenterVertically) {
+                Box(
+                    modifier = Modifier
+                        .size(50.dp) // Adjust the size here
+                        .clip(MaterialTheme.shapes.extraSmall)
+                ) {
 
-                    Box(
-                        modifier = Modifier
-                            .size(50.dp) // Adjust the size here
-                            .clip(MaterialTheme.shapes.extraSmall)
-                    ) {
+                    Checkbox(modifier = Modifier.padding(start = 5.dp),
 
-                        Checkbox(modifier = Modifier.padding(start = 30.dp),
+                        checked = agree,
+                        onCheckedChange = { checked ->
+                            agree = checked
+                        }
+                    )
+                }
+                // "Terms and Conditions" Button
+                Text(
+                    text = "By continuing you accept thea",
+                    fontSize = 8.sp,
+                    color = Color.Black
+                )
 
-                            checked = rememberMe,
-                            onCheckedChange = { checked ->
-                                rememberMe = checked
-                            }
-                        )
+                TextButton(
+                    onClick = {
+                        // Handle "Terms and Conditions" button click
                     }
-
-                    Text(
-                        text = "Remember Me",
-                        fontSize = 12.sp,
-                        color = Color.Black,
-                        modifier = Modifier.padding(start = 10.dp),
-
-                        )
-
-                    TextButton(
-                        modifier = Modifier.padding(start = 160.dp),
-                        onClick = { },
-
-                        ) {
-                        Text("Forgot your Password? ", color = Color.Blue, fontSize = 12.sp)
-                    }
+                ) {
+                    Text("Privacy Policy", color = Color.Blue, fontSize = 8.sp)
                 }
 
+                Text(
+                    text = "and",
+                    fontSize = 12.sp,
+                    color = Color.Black
+                )
 
+                TextButton(
+                    onClick = {
+                        // Handle "Conditions" button click
+                    }
+                ) {
+                    Text("Terms of Use.", color = Color.Blue, fontSize = 8.sp)
+                }
             }
 
 
             Column(
                 modifier = Modifier.fillMaxSize()
                     .fillMaxWidth()
-                    .padding(top = 130.dp)
+                    .padding(top = 20.dp)
                     .background(Color.White), verticalArrangement = Arrangement.Center,
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
@@ -260,7 +345,7 @@ class LogInScreen {
                         .padding(start = 30.dp)
                         .padding(end = 30.dp)
                         .background(
-                            Color.Red,
+                            Color.Blue,
                             shape = MaterialTheme.shapes.medium
                         )
                         .clickable {
@@ -274,19 +359,13 @@ class LogInScreen {
                                 // Handle the click event for the red background here
                             }
                     ) {
-
                         TextButton(
                             onClick = {
                                 // "Next" TextButton click action
                             },
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .background(
-                                    color = Color.Blue, // Change background color to red
-                                    shape = RoundedCornerShape(16.dp) // Rounded corners
-                                ),
-                            // Adjust padding here
-                            colors = ButtonDefaults.textButtonColors(contentColor = Color.White)
+                            colors = ButtonDefaults.textButtonColors(
+                                contentColor = Color.White
+                            )
                         ) {
 
                             Row(
@@ -294,12 +373,12 @@ class LogInScreen {
                                 horizontalArrangement = Arrangement.Center
                             ) {
 
-
                                 Text(
-                                    text = "Login",
+                                    text = "Register",
                                     fontWeight = FontWeight.Bold,
                                     fontSize = 25.sp
                                 )
+
                                 Spacer(modifier = Modifier.width(5.dp))
                                 Icon(
                                     imageVector = Icons.Default.KeyboardArrowRight,
@@ -307,55 +386,54 @@ class LogInScreen {
                                     tint = Color.White, // Change the arrow color
                                     modifier = Modifier.size(35.dp)
                                 )
+
+
                             }
                         }
                     }
                 }
 
-                Row(
-                    modifier = Modifier
-                        .padding(),
-                    // Add vertical scroll if needed
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.SpaceBetween
-                ) {
-                    // Remember Me Checkbox
                     Row(
-                        modifier = Modifier.padding(top = 20.dp),
-                        verticalAlignment = Alignment.CenterVertically
+                        modifier = Modifier
+                            .padding(),
+                             // Add vertical scroll if needed
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.SpaceBetween
                     ) {
-                        Text(
-                            text = "Don't have account yet?",
-                            fontSize = 18.sp,
-                            modifier = Modifier
-                                .padding()
-                        )
+                        // Remember Me Checkbox
+                        Row(
+                            modifier = Modifier.padding(top = 20.dp),
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Text(
+                                text = "Already a member?",
+                                fontSize = 18.sp,
+                                modifier = Modifier
+                                    .padding()
+                            )
 
-                        Text(
-                            text = "Register",
-                            color = Color.Blue,
-                            fontSize = 18.sp,
-                            fontWeight = FontWeight.Bold,
-                            modifier = Modifier
-                                .clickable { /* Handle Register Now Text Click */ }
-                                .padding(start = 5.dp)
-                        )
+                            Text(
+                                text = "Log in",
+                                color = Color.Blue,
+                                fontSize = 18.sp,
+                                fontWeight = FontWeight.Bold,
+                                modifier = Modifier
+                                    .clickable { /* Handle Register Now Text Click */ }
+                                    .padding(start = 5.dp)
+                            )
+                        }
                     }
-                }
+
             }
         }
     }
 
+    fun Text(color: Color, modifier: Modifier) {
 
     }
+}
 
+private fun Alignment.clickable(function: () -> Unit) {
 
+}
 
-
-
-
-
-           
-        
-
-        
