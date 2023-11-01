4ec613e9a19457924209e12aa38404f604d08dc2
cayceevans213 <cevangelista2021@student.nbscollege.edu.ph>
2023-11-01 16:16:29 +0800

    edit

--- app/src/main/java/com/nbscollege/fitnessapp/MainActivity.kt
+++ app/src/main/java/com/nbscollege/fitnessapp/MainActivity.kt
@@ -7,17 +7,26 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.annotation.RequiresApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.nbscollege.fitnessapp.model.SplashLoading
 
 import com.nbscollege.fitnessapp.ui.theme.FitnessAppTheme
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -29,18 +38,23 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
 
+
         setContent {
             FitnessAppTheme {
 
-                // A surface container using the 'background' color from the theme
                     Navigation()
+
+
             }
         }
+
+
     }
 
 
 
 }
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
