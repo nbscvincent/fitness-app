eaf86c64b3eb8b2c19f80a91e64ae5fafca309d2
cayceevans213 <cevangelista2021@student.nbscollege.edu.ph>
2023-11-13 22:26:18 +0800

    edit log in screen and data class for username

---
+++ app/src/main/java/com/nbscollege/fitnessapp/authscreen/model/LogInUser.kt
@@ -0,0 +1,13 @@
+package com.nbscollege.fitnessapp.authscreen.model
+
+data class LogInUser (
+    val username: String,
+    val password: String,
+//    val age: String,
+//    val height: String,
+)
+val account = listOf(
+    LogInUser("Cayce", "Cayce213")
+)
+
+
