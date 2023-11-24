f84acb6c77135dd6b24d02f2c883e7959beb7ed0
cayceevans213 <cevangelista2021@student.nbscollege.edu.ph>
2023-11-24 10:02:40 +0800

    edit

--- app/src/main/java/com/nbscollege/fitnessapp/mainscreen/main/ProfileScreen.kt
+++ app/src/main/java/com/nbscollege/fitnessapp/mainscreen/main/ProfileScreen.kt
@@ -336,8 +336,8 @@ import com.nbscollege.fitnessapp.viewmodel.ScreenViewModel
 
                                         Icon(
                                             imageVector = Icons.Rounded.Edit,
-                                            modifier = Modifier.size(20.dp)
-                                                .padding(end =5.dp)
+                                            modifier = Modifier.size(30.dp)
+                                                .padding(top=15.dp)
                                                 .clickable( onClick = {
 
                                                 }
@@ -363,7 +363,7 @@ import com.nbscollege.fitnessapp.viewmodel.ScreenViewModel
                                     Box(
                                         modifier = Modifier
                                             .height(45.dp)
-                                            .background(Color.LightGray)
+                                            .background(Color.White)
                                             .fillMaxWidth()
                                             .background(Color.LightGray.copy(alpha = 0.1f))
                                     ) {
@@ -640,165 +640,5 @@ import com.nbscollege.fitnessapp.viewmodel.ScreenViewModel
     }
 }
 
-
-
-
-
-
-
-
-
-//                        Text(
-//                            "Username: ${profile.username}",
-//                            color = Color.Black,
-//                            fontSize = 30.sp,
-//                            fontWeight = FontWeight.Thin,
-//                            modifier = Modifier.padding(start = 40.dp, top = 10.dp)
-//                        )
-//
-//                        Text(
-//                            "Height: ${profile.height}",
-//                            color = Color.Black,
-//                            fontSize = 30.sp,
-//                            fontWeight = FontWeight.Thin,
-//                            modifier = Modifier.padding(start = 40.dp, top = 10.dp)
-//                        )
-//                        Text(
-//                            "Weight: ${profile.weight}",
-//                            color = Color.Black,
-//                            fontSize = 30.sp,
-//                            fontWeight = FontWeight.Thin,
-//                            modifier = Modifier.padding(start = 40.dp, top = 10.dp)
-//                        )
-//
-//                        Text(
-//                            "Age: ${profile.age}",
-//                            color = Color.Black,
-//                            fontSize = 30.sp,
-//                            fontWeight = FontWeight.Thin,
-//                            modifier = Modifier.padding(start = 40.dp, top = 10.dp)
-//                        )
-//
-//                        val height = profile.height
-//                        val weight = profile.weight
-//
-//
-//                        if (height != null && weight != null) {
-//
-//                            val bmi = weight / (height * height) * 10000
-////                            val bmiText = String.format(Locale.getDefault(), "BMI: %.2f - $bmiCategory", bmi)
-//                            val bmiCategory = when {
-//                                bmi < 18.5 -> "Underweight"
-//                                bmi < 24.9 -> "Normal Weight"
-//                                bmi < 29.9 -> "Overweight"
-//                                else -> "Obese"
-//                            }
-//                            // Display BMI
-//                            if (bmi < 18.5) {
-//                                Text(
-//                                    "BMI: %.2f".format(bmi),
-//                                    color = Color.Black,
-//                                    fontSize = 30.sp,
-//                                    fontWeight = FontWeight.Thin,
-//                                    modifier = Modifier.padding(start = 40.dp, top = 10.dp)
-//                                )
-//
-//                                Text(
-//                                    "Category: $bmiCategory",
-//                                    color = Color.Black,
-//                                    fontSize = 30.sp,
-//                                    fontWeight = FontWeight.Thin,
-//                                    modifier = Modifier.padding(start = 40.dp, top = 10.dp),
-//                                )
-//
-//                                Text(
-//                                    "Tips: Try to avoid foods with a lot of added sugar, fat and salt, like cakes, takeaway foods and sugary drinks. You can put on weight by eating small meals frequently throughout the day. Try to snack on healthy, high energy foods like cheese, nuts, milk-based smoothies and dried fruit.",
-//                                    color = Color.Red,
-//                                    fontWeight = FontWeight.Bold,
-//                                    modifier = Modifier.padding(start = 40.dp, top = 10.dp)
-//                                )
-//                            } else if (bmi < 24.5) {
-//                                Text(
-//                                    "BMI: %.2f".format(bmi),
-//                                    color = Color.Black,
-//                                    fontSize = 30.sp,
-//                                    fontWeight = FontWeight.Thin,
-//                                    modifier = Modifier.padding(start = 40.dp, top = 10.dp)
-//                                )
-//                                Text(
-//                                    "Category: $bmiCategory",
-//                                    color = Color.Black,
-//                                    fontSize = 30.sp,
-//                                    fontWeight = FontWeight.Thin,
-//                                    modifier = Modifier.padding(start = 40.dp, top = 10.dp)
-//                                )
-//                                Text(
-//                                    "Please Maintain",
-//                                    color = Color.Red,
-//                                    fontWeight = FontWeight.Bold,
-//                                    modifier = Modifier.padding(start = 40.dp, top = 10.dp)
-//                                )
-//                            } else if (bmi < 29.9) {
-//                                Text(
-//                                    "BMI: %.2f".format(bmi),
-//                                    color = Color.Black,
-//                                    fontSize = 30.sp,
-//                                    fontWeight = FontWeight.Thin,
-//                                    modifier = Modifier.padding(start = 40.dp, top = 10.dp)
-//                                )
-//                                Text(
-//                                    "Category: $bmiCategory",
-//                                    color = Color.Black,
-//                                    fontSize = 30.sp,
-//                                    fontWeight = FontWeight.Thin,
-//                                    modifier = Modifier.padding(start = 40.dp, top = 10.dp)
-//                                )
-//                                Text(
-//                                    "Tips: eat a balanced, calorie-controlled diet as recommended by your GP or weight loss management health professional (such as a dietitian) join a local weight loss group. take up activities such as fast walking, jogging, swimming or tennis for 150 to 300 minutes (two-and-a-half to five hours) a week.",
-//                                    color = Color.Red,
-//                                    fontSize = 30.sp,
-//                                    modifier = Modifier.padding(start = 40.dp, top = 10.dp)
-//                                )
-//                            } else {
-//                                Text(
-//                                    "BMI: %.2f".format(bmi),
-//                                    color = Color.Black,
-//                                    fontSize = 30.sp,
-//                                    fontWeight = FontWeight.Thin,
-//                                    modifier = Modifier.padding(start = 40.dp, top = 10.dp)
-//                                )
-//                                Text(
-//                                    "Category: $bmiCategory",
-//                                    color = Color.Black,
-//                                    fontSize = 30.sp,
-//                                    fontWeight = FontWeight.Thin,
-//                                    modifier = Modifier.padding(start = 40.dp, top = 10.dp)
-//                                )
-//                            }
-//
-////                            Text(
-////                                "BMI: %.2f".format(bmi),
-////                                color = Color.Black,
-////                                fontSize = 30.sp,
-////                                fontWeight = FontWeight.Thin,
-////                                modifier = Modifier.padding(start = 40.dp, top = 10.dp)
-////                            )
-////                            Text(
-////                                "Category: $bmiCategory",
-////                                color = Color.Black,
-////                                fontSize = 30.sp,
-////                                fontWeight = FontWeight.Thin,
-////                                modifier = Modifier.padding(start = 40.dp, top = 10.dp)
-////                            )
-//                        } else {
-//                            // Handle the case where height or weight couldn't be converted to numbers
-//                            Text(
-//                                "BMI: Invalid height or weight",
-//                                color = Color.Red, // or any color you prefer for error messages
-//                                fontSize = 30.sp,
-//                                fontWeight = FontWeight.Thin,
-//                                modifier = Modifier.padding(start = 40.dp, top = 10.dp)
-//                            )
-//                        }
 
 
