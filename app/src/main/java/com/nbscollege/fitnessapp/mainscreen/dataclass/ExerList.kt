8e6451018aea3aedb4e54e8386ecd13dfb8c0639
forgetfulpirate <147906395+forgetfulpirate@users.noreply.github.com>
2023-11-26 00:29:07 +0800

    EDIT

--- app/src/main/java/com/nbscollege/fitnessapp/mainscreen/dataclass/ExerList.kt
+++ app/src/main/java/com/nbscollege/fitnessapp/mainscreen/dataclass/ExerList.kt
@@ -9,11 +9,11 @@ data class ExerList(
 )
 
 val ExerciseList = listOf(
-    ExerList(title = "JUMPING JACKS", time = "00:45", route = CategoryRoute.ABS.name),
+    ExerList(title = "PLANK", time = "00:45", route = CategoryRoute.ABS.name),
     ExerList(title = "ABDOMINAL CRUNCHES", time = "01:00",route = CategoryRoute.ABS.name),
-    ExerList(title = "RUSSIAN TWIST",time = "01:30",route = CategoryRoute.ABS.name),
-    ExerList(title = "MOUNTAIN CLIMBER",time = "01:45",route = CategoryRoute.ABS.name),
+    ExerList(title = "RUSSIAN TWIST",time = "01:15",route = CategoryRoute.ABS.name),
+    ExerList(title = "MOUNTAIN CLIMBER",time = "01:30",route = CategoryRoute.ABS.name),
     ExerList(title = "HEEL TOUCH",time = "01:45",route = CategoryRoute.ABS.name),
     ExerList(title = "LEG RAISES",time = "01:45",route = CategoryRoute.ABS.name),
-    ExerList(title = "PLANK",time = "00:20",route = CategoryRoute.ABS.name),
+    ExerList(title = "BICYCLE CRUNCHES",time = "01:00",route = CategoryRoute.ABS.name),
 )
