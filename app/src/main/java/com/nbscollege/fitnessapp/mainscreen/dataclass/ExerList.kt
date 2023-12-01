8a2f235f634e7051090e26ae306b59c9ea209a96
forgetfulpirate <147906395+forgetfulpirate@users.noreply.github.com>
2023-12-01 21:48:26 +0800

    edit

--- app/src/main/java/com/nbscollege/fitnessapp/mainscreen/dataclass/ExerList.kt
+++ app/src/main/java/com/nbscollege/fitnessapp/mainscreen/dataclass/ExerList.kt
@@ -17,7 +17,14 @@ val ExerciseList = listOf(
     ExerList(title = "HEEL TOUCH",time = "01:45",route = CategoryRoute.ABS.name),
     ExerList(title = "LEG RAISES",time = "01:45",route = CategoryRoute.ABS.name),
     ExerList(title = "BICYCLE CRUNCHES",time = "01:00",route = CategoryRoute.ABS.name),
-
+//for chest workout
+    ExerList(title = "PLANKTON", time = "00:45", route = CategoryRoute.CHEST.name),
+    ExerList(title = "ABDOMINAL CRUNCHES", time = "01:00",route = CategoryRoute.CHEST.name),
+    ExerList(title = "RUSSIAN TWIST",time = "01:15",route = CategoryRoute.CHEST.name),
+    ExerList(title = "MOUNTAIN CLIMBER",time = "01:30",route = CategoryRoute.CHEST.name),
+    ExerList(title = "HEEL TOUCH",time = "01:45",route = CategoryRoute.CHEST.name),
+    ExerList(title = "LEG RAISES",time = "01:45",route = CategoryRoute.CHEST.name),
+    ExerList(title = "BICYCLE CRUNCHES",time = "01:00",route = CategoryRoute.CHEST.name),
 
     //for arm workout
     ExerList(title = "PLANKTON", time = "00:45", route = CategoryRoute.ARM.name),
--- app/src/main/java/com/nbscollege/fitnessapp/mainscreen/exercisescreen/ChestsScreen.kt
+++ app/src/main/java/com/nbscollege/fitnessapp/mainscreen/exercisescreen/ChestsScreen.kt
@@ -1,26 +1,43 @@
 package com.nbscollege.fitnessapp.mainscreen.exercisescreen
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowLeft
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SmallFloatingActionButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.zIndex
 import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavController
+import com.nbscollege.fitnessapp.R
+import com.nbscollege.fitnessapp.mainscreen.categorycard.ExerCateg
+import com.nbscollege.fitnessapp.mainscreen.dataclass.ExerciseList
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
@@ -29,21 +46,50 @@ fun ChestScreen(navController: NavController, backStackEntry: NavBackStackEntry)
 
     Scaffold(
         topBar = {
-
             Box(
                 modifier = Modifier
-                    .height(50.dp).background(Color.White).fillMaxWidth()
+//                    .height(50.dp)
+                    .background(Color.White)
+                    .fillMaxWidth()
             ) {
-                Text(
-                    text = "CHEST WORKOUT",
-                    fontWeight = FontWeight.Bold,
-                    fontSize = 25.sp,
-                    color = Color.Black,
-                    modifier = Modifier.graphicsLayer(translationY = 25f, translationX = 30f)
+
+//                IconButton(onClick = { navController.navigateUp() }) {
+//                    Icon(
+//                        imageVector = Icons.Rounded.ArrowBackIos,
+//                        modifier = Modifier
+//                            .size(30.dp)
+//                            .padding(start = 5.dp, end = 5.dp)
+//                            .zIndex(3f),
+//                        contentDescription = "Back",
+//                        tint = Color.White
+//                    )
+//                }
+                SmallFloatingActionButton(
+                    onClick = {
+                        navController.navigate("HomeScreen")
+                    },
+                    containerColor = Color.Transparent,
+                    modifier = Modifier
+                        .padding(start = 5.dp, end = 5.dp)
+                        .zIndex(3f)
+
+                ) {
+                    Icon(
+                        Icons.Filled.KeyboardArrowLeft, "Back",
+                        modifier = Modifier.size(40.dp),
+                        tint = Color.White
+                    )
+                }
+
+                Image(
+                    painter = painterResource(id = R.drawable.interchest),
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(150.dp),
                 )
-
             }
-
         },
 //        bottomBar = {
 //            BottomAppBar {
@@ -51,6 +97,8 @@ fun ChestScreen(navController: NavController, backStackEntry: NavBackStackEntry)
 //            }
 //        }
     ) { innerPadding ->
+        // Content of your screen goes here
+
         Column(
             modifier = Modifier
                 .background(Color.White)
@@ -58,8 +106,35 @@ fun ChestScreen(navController: NavController, backStackEntry: NavBackStackEntry)
                 .padding(innerPadding)
         ) {
 
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
 
+//                Spacer(modifier = Modifier.height(20.dp))
+                LazyColumn(
+                    modifier = Modifier.padding(16.dp),
+                    verticalArrangement = Arrangement.spacedBy(10.dp),
+                ) {
 
+                    item {
+                        Spacer(modifier = Modifier.height(20.dp))
+                        Text(
+                            text = "EXERCISES",
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 20.sp,
+                            color = Color.Black,
+                            modifier = Modifier
+                        )
+                    }
+
+                    items(ExerciseList) { ExerList ->
+                        ExerCateg(exer = ExerList, navController)
+                    }
+
+                }
+            }
 
         }
     }
