fdf5804393ac1a4b1ef3909eccbb894ebc09bb00
cayceevans213 <cevangelista2021@student.nbscollege.edu.ph>
2024-01-07 22:26:41 +0800

    edit

--- app/src/main/java/com/nbscollege/fitnessapp/mainscreen/settingscreen/GeneralSettings.kt
+++ app/src/main/java/com/nbscollege/fitnessapp/mainscreen/settingscreen/GeneralSettings.kt
@@ -9,18 +9,15 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Email
-import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -28,7 +25,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.text.font.FontWeight
@@ -36,6 +32,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavController
+
 
 @Composable
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
@@ -83,21 +80,26 @@ fun GeneralSettings(navController: NavController, backStackEntry: NavBackStackEn
 
             OutlinedTextField(
 
-                modifier = Modifier.absolutePadding(left = 20.dp, bottom = 11.dp),
+                modifier = Modifier.fillMaxWidth().padding(start=20.dp, end=20.dp),
 
                 label = { Text("Current User", fontWeight = FontWeight.Medium) },
                 value = currentPassword,
                 singleLine = true,
                 onValueChange = { currentPassword = it },
-                trailingIcon = {
+                leadingIcon = {
                     Icon(
                         Icons.Rounded.Email,
                         contentDescription = "Username"
                     )
+
                 },
                 shape = RoundedCornerShape(12.dp),
-
-                )
+                colors = TextFieldDefaults.outlinedTextFieldColors(
+                    focusedBorderColor = Color.Gray,
+                    unfocusedBorderColor = Color.Gray,
+                    textColor = Color.Black
+                ),
+            )
 
             Text("New Password")
 
